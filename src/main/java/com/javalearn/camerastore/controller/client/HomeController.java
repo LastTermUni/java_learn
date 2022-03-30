5050ca32146e418b481479557fcc6c1f9325add4
PhuocNguyenG <73028315+PhuocNguyenG@users.noreply.github.com>
2022-03-31 03:27:42 +0700

    .

--- src/main/java/com/javalearn/camerastore/controller/client/HomeController.java
+++ src/main/java/com/javalearn/camerastore/controller/client/HomeController.java
@@ -1,49 +1,70 @@
 package com.javalearn.camerastore.controller.client;
 
+import com.javalearn.camerastore.service.CategoryService;
+import com.javalearn.camerastore.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-public class HomeController {
+import javax.servlet.ServletContext;
 
-    @RequestMapping(value = "/home", method = RequestMethod.GET)
+@RestController
+@RequestMapping(value = "/")
+public class HomeController {
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    ServletContext context;
+
+    @GetMapping("home")
     public ModelAndView homePage() {
         ModelAndView mav = new ModelAndView("client/home");
+        mav.addObject("products", productService.getProduct());
+        mav.addObject("cates",categoryService.getCategory());
         return mav;
     }
-    @RequestMapping(value = "/product-detail",method = RequestMethod.GET)
-    public ModelAndView productDetail(){
+
+    @GetMapping(value = "product-detail")
+    public ModelAndView productDetail() {
         return new ModelAndView("client/product-detail");
     }
-    @RequestMapping(value = "/product",method = RequestMethod.GET)
-    public ModelAndView product(){
+
+    @RequestMapping(value = "product", method = RequestMethod.GET)
+    public ModelAndView product() {
         return new ModelAndView("client/product");
     }
 
-//    giỏ hàng
-    @RequestMapping(value = "/cart",method = RequestMethod.GET)
-    public ModelAndView cart(){
+    // giỏ hàng
+    @RequestMapping(value = "cart", method = RequestMethod.GET)
+    public ModelAndView cart() {
         return new ModelAndView("client/cart");
     }
 
-    @RequestMapping(value = "/checkout",method = RequestMethod.GET)
-    public ModelAndView checkout(){
+    @RequestMapping(value = "checkout", method = RequestMethod.GET)
+    public ModelAndView checkout() {
         return new ModelAndView("client/checkout");
     }
-    @RequestMapping(value = "/register",method = RequestMethod.GET)
-    public ModelAndView register(){
+
+    @RequestMapping(value = "register", method = RequestMethod.GET)
+    public ModelAndView register() {
         return new ModelAndView("client/register");
     }
 
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
-    public ModelAndView login(){
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public ModelAndView login() {
         return new ModelAndView("client/login");
     }
 
-    @RequestMapping(value = "/404",method = RequestMethod.GET)
-    public ModelAndView error(){
+    @RequestMapping(value = "404", method = RequestMethod.GET)
+    public ModelAndView error() {
         return new ModelAndView("client/error");
     }
 }
